3843911681a3fe0c25e227a375870d6301a8d035
Dobroskok <dobrnicke@gmail.com>
2020-07-21 23:49:45 +0300

    rolling number is 2

--- dice_roller.py
+++ dice_roller.py
@@ -1,6 +1,9 @@
-def main():
-  print('You rolled a die')
+import random
 
-if __name__== "__main__":
+
+def main():
+  roll = random.randint(1,6)
+  print(f'You rolled a {roll}')
+
+if __name__ == "__main__":
   main()
-  
